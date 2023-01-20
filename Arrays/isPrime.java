d7df48c66c46f0925e2793780d13ef31adbb20a2
rspraneeth <rspraneeth.rsp@gmail.com>
2023-01-20 10:57:57 -0600

    Prime number program

--- Arrays/isPrime.java
+++ Arrays/isPrime.java
@@ -2,21 +2,17 @@ import java.util.Scanner;
 
 public class isPrime {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter a number to check if it is prime or not: ");
+    	Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        boolean flag = false;
-        for(int i = 2; i<n;i++){
-            if (n%i == 0){
-                flag = true;
-                break;
-            }
-        }
-        if (flag){
-            System.out.print("NO");
-        }
-        else{
-            System.out.print("YES");
-        }
+        int count = 0;
+        for(int i = 1; i <= n; i++) if (n%i == 0) count++;
+        if (count == 2) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
+
+/*Take an integer A as input, you have to tell whether it is a prime number or not.
+
+A prime number is a natural number greater than 1 which is divisible only by 1 and itself.
+
+So it has only 2 factors.*/
