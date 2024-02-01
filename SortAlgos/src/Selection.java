af62a1fed0c77803e7c63b8a71b2f7915c6a27e2
rspraneeth <rspraneeth.rsp@gmail.com>
2024-01-31 19:44:30 -0800

    selection sort

--- SortAlgos/src/Selection.java
+++ SortAlgos/src/Selection.java
@@ -3,6 +3,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Selection {
+
+    /*Selection sort is a simple and efficient sorting algorithm that works
+    by repeatedly selecting the smallest (or largest) element from the
+    unsorted portion of the list and moving it to the sorted portion of the list. */
     public static void main(String[] args) {
         int[] nums = {6, 5, 2, 8, 9, 4};
 
@@ -12,17 +16,20 @@ public class Selection {
         }
 
         int size = nums.length;
-        int min = 0;
+        int mini = 0, temp;
         for (int i = 0; i < size - 1; i++){
-            min = nums[i];
+
+            mini = i; //consider as min index value
 
             for (int j = i; j < size - 1; j++){
-                if (nums[j+1] < min){
-                    min = nums[j+1];
-                    nums[j+1] = nums[i];
-                    nums[i] = min;
+                if (nums[j+1] < nums[mini]){
+                    mini = j+1; //finding min index value for the iteration
                 }
             }
+
+            temp = nums[mini]; // taking min value in temp to place in sorted part
+            nums[mini] = nums[i];
+            nums[i] = temp; //placing in the end of sorted part
         }
 
 
